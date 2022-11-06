6cf2eebe25106ab26ea3d2f94e96c72aa36bc7aa
Mary Elenius <mary.panda.jackson@gmail.com>
2022-11-05 22:48:13 -0400

    cleaned up geocall

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // Global variables
-var cityState = "";
+var city = "";
 const apiKey = "2a486f5bbf138ec48900a8c6c87fef7c"; // search history as an empty array
 var search = "";
 var lat;
@@ -7,35 +7,44 @@ var lon;
 // Weather API endpoint
 var onecallUrl =
   "https://api.openweathermap.org/data/3.0/onecall?q=" +
-  cityState +
+  city +
   "&units=imperial&appid=" +
   apiKey;
 
 // DOM element references
-// search form
+// search button
 const buttonSearch = document.getElementById("searchBtn");
 // search input
 const inputSearch = document.getElementById("inputBox");
 // history button
 const buttonRecent = document.getElementById("recentBtn");
-// container/section for today's weather
+// container for today's weather
 const currentContainer = document.getElementById("currentConditions");
-// container/section for the forecast
+// container for the forecast
 const forecastContainer = document.getElementById("forecast");
-// search history container
+// container for the search history
 const historyContainer = document.getElementById("historyContainer");
 
 // Function to display the search history list.
 function renderSearchHistory() {
-  // empty the search history container
-  // loop through the history array creating a button for each item
-  // append to the search history container
+  buttonRecent.innerHTML = "";
+  // Creates forloop for search history
+  for (var i = 0; i < localStorage.length; i++) {
+    city = localStorage.getItem(localStorage.key(i));
+    // Creates button for search history
+    var historyButton = document.createElement("button");
+    // Sets id for generated button to recentBtn so match css
+    historyButton.setAttribute("class", "button");
+    historyButton.setAttribute("id", "recentBtn", city);
+    historyButton.textContent = city;
+    buttonRecent.appendChild(historyButton);
+  }
 }
 
 // Function to update history in local storage then updates displayed history.
 function appendToHistory(search) {
   // push search term into search history array
-
+  localStorage.setItem(search, search);
   // set search history array to local storage
   renderSearchHistory();
 }
@@ -49,7 +58,7 @@ function initSearchHistory() {
 }
 
 // Function to display the CURRENT weather data fetched from OpenWeather api.
-function renderCurrentWeather(cityState, weather) {
+function renderCurrentWeather(city, weather) {
   // Store response data from our fetch request in variables
   // temperature, wind speed, etc.
   // document.create the elements you'll want to put this information in
@@ -82,46 +91,67 @@ function renderForecast(dailyForecast) {
   }
 }
 
-function renderItems(cityState, data) {
-  renderCurrentWeather(cityState, data.list[0]);
+function renderItems(city, data) {
+  renderCurrentWeather(city, data.list[0]);
   renderForecast(data.list);
 }
 
 // Fetches weather data for given location from the Weather Geolocation
 // endpoint; then, calls functions to display current and forecast weather data.
-function fetchWeather(location) {
-  // varialbles of longitude, latitude, cityState name - coming from location
+function fetchWeather(lat, lon, city) {
   // api url
-  // fetch, using the api url, .then that returns the response as json, .then that calls renderItems(cityState, data)
-}
-
-function fetchCoords(search) {
-  var cityState = search;
-  var coordsUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    cityState +
-    ",US&limit=5&appid=" +
+  var onecallUrl =
+    "https://api.openweathermap.org/data/3.0/onecall?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
     apiKey;
 
-  fetch(coordsUrl).then(function (response) {
-    console.log(response);
-    console.log(response.statusText);
+  // fetch, using the api url, .then that returns the response as json, .then that calls renderItems(cityState, data)
+  fetch(onecallUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
+        renderItems(city, data);
+      });
+    } else {
+      return;
+    }
+  });
+}
+
+// Function to fetch coordinates
+function fetchCoords(search) {
+  var city = search;
+  var coordsUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    city +
+    ",US&limit=5&appid=" +
+    apiKey;
+  // API fetch for coordinates
+  fetch(coordsUrl).then(function (response) {
+    if (response.ok) {
+      // Logs data from API fetch
+      response.json().then(function (data) {
         if (data.length === 0) {
           return;
         }
+        console.log(data);
+        // Creates variables from lat and long values
         var lat = data[0].lat;
         var lon = data[0].lon;
+        search = data[0].name;
+        // Logs values for long and lat
         console.log("latitude: " + lat + "\nlongitude: " + lon);
 
+        // Appends search to history and runs fetch weather function with created variables from API response
         appendToHistory(search);
-        fetchWeather(lat, lon, cityState);
+        fetchWeather(lat, lon, city);
       });
     }
   });
 }
+
 function handleSearchFormSubmit(e) {
   console.log("Search submitted");
   if (!inputSearch.value) {
@@ -130,19 +160,17 @@ function handleSearchFormSubmit(e) {
   e.preventDefault();
 
   console.log("Search = " + inputSearch.value);
-  var search = inputSearch.value.trim();
+  search = inputSearch.value.trim();
   fetchCoords(search);
   inputSearch.value = "";
 }
 
 function handleSearchHistoryClick(e) {
   console.log("History called");
-  console.log("cityState: = " + e.target.textContent);
-  var search = e.target.textContent;
-
+  console.log("City: = " + e.target.textContent);
+  search = e.target.textContent;
   fetchCoords(search);
 }
 
 initSearchHistory();
 buttonSearch.addEventListener("click", handleSearchFormSubmit);
-buttonRecent.addEventListener("click", handleSearchHistoryClick);
